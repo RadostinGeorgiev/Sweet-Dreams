aba39a6a674c2e623e8bd7565d7eb1e74c32d4fd
Radostin Georgiev <radostin.r.georgiev@gmail.com>
2025-03-15 09:18:35 +0200

    Create simple gallery

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 
-import { Container } from "@mantine/core";
+import { Container, Image } from "@mantine/core";
+import { Carousel } from "@mantine/carousel";
+import "@mantine/carousel/styles.css";
 
 import BlogMasonry from "./BlogMasonry/BlogMasonry";
 import BlogList from "./BlogList/BlogList";
@@ -8,6 +10,7 @@ import BlogList from "./BlogList/BlogList";
 export default function Main() {
   const [articles, setArticles] = useState([]);
   const [users, setUsers] = useState([]);
+  const [images, setImages] = useState([]);
 
   useEffect(() => {
     fetch("https://dummyjson.com/c/f4c3-1f33-4174-878d")
@@ -21,6 +24,12 @@ export default function Main() {
       .then((data) => {
         setUsers(data.users);
       });
+
+    fetch("https://dummyjson.com/recipes?limit=20&select=image")
+      .then((response) => response.json())
+      .then((data) => {
+        setImages(data.recipes);
+      });
   }, []);
 
   return (
@@ -30,6 +39,21 @@ export default function Main() {
         <h1>Sweet Dreams</h1>
         <BlogMasonry articles={articles} users={users} />
         <BlogList articles={articles} users={users} />
+        <Carousel height={300} slideSize="md" slideGap="xs" loop dragFree>
+          {images.map((src) => {
+            return (
+              <Carousel.Slide key={src.id}>
+                <Image
+                  h="auto"
+                  w={200}
+                  src={src.image}
+                  alt={`Image ${src.id}`}
+                  radius="0"
+                />
+              </Carousel.Slide>
+            );
+          })}
+        </Carousel>
       </Container>
     </>
   );
