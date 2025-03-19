ffbad782d6625a2244887f17f352d3065aad2f1e
Radostin Georgiev <radostin.r.georgiev@gmail.com>
2025-03-19 20:28:57 +0200

    Add local images

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const endpoints = {
   users: "/users",
-  items: "/c/f4c3-1f33-4174-878d",
+  items: "/c/401c-ac78-4085-93d7",
   recipes: "/recipes",
   limitedRecipeImages: "/recipes?limit=20&select=image",
 };
