11504feba1efd6ad97791a1be5efff74ee0ccdb2
Radostin Georgiev <radostin.r.georgiev@gmail.com>
2025-03-21 07:37:10 +0200

    Move providers in main file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router";
+import { useEffect } from "react";
+import { Routes, Route, useLocation, useNavigate } from "react-router";
 
-import { MantineProvider, AppShell } from "@mantine/core";
+import { AppShell } from "@mantine/core";
 import "@mantine/core/styles.css";
 
 import { services } from "../services/item.service";
@@ -8,7 +9,6 @@ import { useFetch } from "../hooks/useFetch";
 import { endpoints } from "../../config";
 
 import "./App.scss";
-import { theme } from "../assets/styles/theme";
 
 import Header from "./Header/Header";
 import LoginForm from "./Login";
@@ -22,6 +22,15 @@ import ProjectDescription from "./ProjectDescription";
 import SinglePost from "./SinglePost/SinglePost";
 
 export default function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (location.pathname !== "/") {
+      navigate("/", { replace: true });
+    }
+  }, []);
+
   const {
     data: articles,
     loading: articlesLoading,
@@ -63,43 +72,39 @@ export default function App() {
   };
 
   return (
-    <BrowserRouter>
-      <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
-        <AppShell header={{ height: 100 }} footer={{ height: 60 }}>
-          <AppShell.Header>
-            <Header />
-          </AppShell.Header>
+    <AppShell header={{ height: 100 }} footer={{ height: 60 }}>
+      <AppShell.Header>
+        <Header />
+      </AppShell.Header>
 
-          <AppShell.Main>
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  <HomePage articles={articles} users={users} images={images} />
-                }
-              />
-              <Route path="/login" element={<LoginForm />} />
-              <Route
-                path="/register"
-                element={<RegisterForm onAddUser={handleAddUser} />}
-              />
+      <AppShell.Main>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <HomePage articles={articles} users={users} images={images} />
+            }
+          />
+          <Route path="/login" element={<LoginForm />} />
+          <Route
+            path="/register"
+            element={<RegisterForm onAddUser={handleAddUser} />}
+          />
 
-              <Route
-                path="/blog"
-                element={<Blog articles={articles} users={users} />}
-              />
-              <Route path="/blog/:id" element={<SinglePost />} />
-              <Route path="/recipes" element={<Recipes />} />
-              <Route path="/tips" element={<CookingTips />} />
-              <Route path="/project" element={<ProjectDescription />} />
-            </Routes>
-          </AppShell.Main>
+          <Route
+            path="/blog"
+            element={<Blog articles={articles} users={users} />}
+          />
+          <Route path="/blog/:id" element={<SinglePost />} />
+          <Route path="/recipes" element={<Recipes />} />
+          <Route path="/tips" element={<CookingTips />} />
+          <Route path="/project" element={<ProjectDescription />} />
+        </Routes>
+      </AppShell.Main>
 
-          <AppShell.Footer>
-            <Footer />
-          </AppShell.Footer>
-        </AppShell>
-      </MantineProvider>
-    </BrowserRouter>
+      <AppShell.Footer>
+        <Footer />
+      </AppShell.Footer>
+    </AppShell>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 // import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import { MantineProvider } from "@mantine/core";
 import App from "./components/App.jsx";
+import { theme } from "./assets/styles/theme";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
-  <App />
+  <BrowserRouter>
+    <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
+      <App />
+    </MantineProvider>
+  </BrowserRouter>
   // </StrictMode>
 );
